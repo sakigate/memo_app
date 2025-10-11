11259bb23bc92cc280ec202f5e1e5f0816843bf4
sakigate <saki_ato_saki_1112@yahoo.co.jp>
2025-10-11 18:11:22 +0900

    削除機能追加

--- resources/views/livewire/memos/show.blade.php
+++ resources/views/livewire/memos/show.blade.php
@@ -10,6 +10,11 @@ $edit = function () {
     return redirect()->route("memos.edit", $this->memo);
 };
 
+$destroy = function(){
+    $this->memo->delete();
+    return redirect()->route("memos.index", $this->memo);
+};
+
 ?>
 
 <div>
@@ -18,4 +23,5 @@ $edit = function () {
     <p>{!! nl2br(e($memo->body))!!}</p>
     
     <button wire:click="edit">編集する</button>
+    <button wire:click="destroy" wire:confirm="本当に削除しますか？">削除する</button>
 </div>
