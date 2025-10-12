c4cd650c7a336f0a22e4c673c4a8b21413b4d269
sakigate <saki_ato_saki_1112@yahoo.co.jp>
2025-10-12 09:33:41 +0900

    バリデーション追加

--- resources/views/livewire/memos/edit.blade.php
+++ resources/views/livewire/memos/edit.blade.php
@@ -1,6 +1,6 @@
 <?php
 
-use function Livewire\Volt\{state, mount};
+use function Livewire\Volt\{state, mount, rules};
 use App\Models\Memo;
 
 //フォームの状態を管理
@@ -13,7 +13,13 @@ mount(function (Memo $memo) {
     $this->body = $memo->body;
 });
 
+rules([
+    'title' => 'required|string|max:50',
+    'body' => 'required|string|max:2000',
+]);
+
 $update = function () {
+    $this->validate();
     $this->memo->update($this->all());
     return redirect()->route('memos.show', $this->memo);
 };
@@ -26,11 +32,19 @@ $update = function () {
 
     <form wire:submit="update">
         <p>
-            <label for="title">タイトル</label><br>
+            <label for="title">タイトル</label>
+            @error('title')
+                <span class="error">({{ $message }})</span>
+            @enderror
+            <br>
             <input type="text" wire:model="title" id="title">
         </p>
         <p>
-            <label for="body">本文</label><br>
+            <label for="body">本文</label>
+            @error('body')
+                <span class="error">({{ $message }})</span>
+            @enderror
+            <br>
             <textarea wire:model="body" id="body"></textarea>
         </p>
         <button type="submit">更新</button>
